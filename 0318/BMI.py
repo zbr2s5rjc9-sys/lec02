4f4a7e1d49f66e6e74fb441e93edb22c101df9ce
rmsdud614 <zbr2s5rjc9@privaterelay.appleid.com>
2026-04-06 13:46:28 +0900

    Add files via upload

---
+++ 0318/BMI.py
@@ -0,0 +1,13 @@
+weight = float(input("몸무게(kg)를 입력해보세요 : "))
+height = float(input("키(cm)를 입력해보세요 : "))
+
+height_m = height / 100
+BMI = weight / (height_m **2)
+
+print(BMI)
+print(f"나의 BMI 지수는 {BMI}다.")
+
+if 23<= BMI <= 24.9:print("비만 전단계입니다!")
+elif 25<= BMI <= 29.9:print("비만 1단계입니다!")
+elif 30<= BMI <= 34.9:print("비만 2단계입니다!")
+elif 35<= BMI:print("비만 3단계입니다!")
